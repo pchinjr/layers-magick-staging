8e527284fbfcd23673b066bca9ca03feb665463f
Paul Chin Jr <pchinjr@gmail.com>
2020-08-03 18:36:42 -0400

    Update index.js

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,17 +1,6 @@
 let arc = require('@architect/functions')
-
 const { execSync } = require("child_process");
 
-async function resizeAvatar(req, context,) {
-
+exports.handler = async (event) => {
   execSync('gm version', { encoding: 'utf8', stdio: 'inherit' })
-  
-  console.log(req.body)
-  
-  return {
-    statusCode: 200,
-    body: "praise cage"
-  }
 }
-
-exports.handler = arc.http.async(resizeAvatar)
